cf140dd01f380a7cb47ee6766f346ae13490988a
aavidad <avidad@dipgra.es>
2026-02-21 15:55:06 +0100

    systray: usar logo_firma.png como icono embebido

--- pkg/applog/logo.go
+++ pkg/applog/logo.go
@@ -1,4 +1,6 @@
 package applog
 
-// LogoRaw represents a generic tiny logo for systray fallback
-var LogoRaw = []byte{0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a /* fake png to compile */}
+import _ "embed"
+
+//go:embed assets/logo_firma.png
+var LogoRaw []byte
